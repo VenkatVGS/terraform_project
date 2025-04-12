9fd761f077926a1936c973b5629cbf537d564dac
VenkatVGS <venkat.tendulkar@gmail.com>
2025-04-12 10:58:08 +0530

    Remote Added

--- Modules_project/provider.tf
+++ Modules_project/provider.tf
@@ -5,7 +5,17 @@ terraform {
       version = "~> 4.0"
     }
   }
+    backend "remote" {
+    hostname = "venkatesh0608.scalr.io"
+    organization = "venkatesh0608"
+
+    workspaces {
+      name = "modules"
+    }
+  }
 }
+
+
 
 # Configure the AWS Provider
 provider "aws" {
